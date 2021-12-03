6526da416d25751f297290dd6390fe672195c994
zai0811 <39562291+zai0811@users.noreply.github.com>
2021-12-03 01:31:30 -0300

    migraciones - autenticacion

---
+++ database/migrations/2021_12_03_040912_create_user_empresa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserEmpresaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_empresa', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('pk_users')->nullable();
+            $table->foreign('pk_users', 'pk_users')->references('id')->on('users');
+            $table->unsignedInteger('pk_empresa')->nullable();
+            $table->foreign('pk_empresa', 'pk_empresa')->references('id')->on('empresa');
+            $table->string('rol')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_empresa');
+    }
+}
